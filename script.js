5110c5eeee812a1bb1ae58305cd471bdfb60feef
isazafeer <isazafeer2003@gmail.com>
2024-02-05 14:38:33 +0000

    added in 'var' for characters

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var ippercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "0123456789"
+  var special = "!@#$%&*?"
+
+  var lowercasePref = confirm("Would you like lowercase characters?");
+  var lowercasePref = confirm("Would you like uppercase characters?");
+  var lowercasePref = confirm("Would you like numerical characters?");
+  var specialPref = confirm("Would you like special characters?");
+
+  var passwordLength = prompt("How many characters should your password be?");
+  
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
